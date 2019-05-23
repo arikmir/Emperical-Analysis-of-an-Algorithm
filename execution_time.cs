a404f470a41bb2a77129f1bd8d6db814019bb5d3
Arik Intenam Mir <mirarikintenam@gmail.com>
2019-05-24 01:07:11 +1000

    Add files via upload

---
+++ execution_time.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+
+namespace execution_time
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int totalRunningTimes = 20;
+            Stopwatch sw = new Stopwatch();
+            var csv = new StringBuilder();
+            for (int size = 1000; size <= 20000; size += 1000)
+            {
+                long totalMilliSecs = 0;
+                double averageMilliSecs = 0;
+                for (int i = 1; i <= totalRunningTimes; i++)
+                {
+                    long milliSecs = 0;
+                    int[] A = GenerateRandomArray(size);
+                    sw.Start();
+                    Median(A);
+                    sw.Stop();
+                    milliSecs = sw.ElapsedMilliseconds;
+                    totalMilliSecs = totalMilliSecs + milliSecs;
+                }
+
+                averageMilliSecs = totalMilliSecs * 1.0 / totalRunningTimes;
+                Console.WriteLine("Size: " + size + "; Average Running Time (MilliSec)= " + averageMilliSecs);
+                var first = size.ToString();
+                var second = averageMilliSecs.ToString();
+                var newLine = string.Format("{0},{1}", first, second);
+                csv.AppendLine(newLine);
+            }
+            File.WriteAllText(@"C:\Users\Arik\Desktop\CAB 301\intenam.csv", csv.ToString());
+            Console.ReadKey();
+        }
+
+        static int[] GenerateRandomArray(int size)
+        {
+            int[] A = new int[size];
+
+            int seed = (int)DateTime.Now.Ticks;
+            Random rnd = new Random(seed);
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                A[i] = rnd.Next(Int32.MaxValue);
+            }
+            return A;
+        }
+
+        static int Median(int[] A)
+        {
+            int n = A.Length;
+            int k = n / 2;
+
+            for (int i = 0; i <= (n - 1); i++)
+            {
+                int numsmaller = 0;
+                int numequal = 0;
+
+                for (int j = 0; j <= (n - 1); j++)
+                {
+
+                    if (A[j] < A[i])
+                    {
+                        numsmaller++;
+                    }
+                    else if (A[j] == A[i])
+                    {
+                        numequal++;
+                    }
+                }
+                if ((numsmaller < k) && (k <= (numsmaller + numequal)))
+                {
+                    return A[i];
+                }
+            }
+            return -1;
+
+        }
+
+    }
+}
---
+++ functionality_test.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asssesment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FunctionalTesting();
+            Console.ReadKey();
+        }
+
+        static int Median(int[] Arr, int n, int k)
+        {
+
+            for (int i = 0; i <= n - 1; i++)
+            {
+                int numsmaller = 0;
+                int numequal = 0;
+
+                for (int j = 0; j <= n - 1; j++)
+                {
+
+                    if (Arr[j] < Arr[i])
+                    {
+                        numsmaller++;
+                    }
+                    else if (Arr[j] == Arr[i])
+                    {
+                        numequal++;
+                    }
+                }
+                if ((numsmaller < k) && (k <= (numsmaller + numequal)))
+                {
+                    return Arr[i];
+                }
+            }
+            return 0;
+        }
+
+        static void FunctionalTesting()
+        {
+            Console.WriteLine("FUNCTIONALITY CORRECTNESS TESTING");
+            Console.WriteLine("----------------------");
+
+            // Console.WriteLine("");
+            int n = 10;
+            int k = (int)Math.Ceiling(n / 2.0);
+
+            //Array of Consecutive Numbers
+            int[] consecutive_arr = new int[] { 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 };
+            int consec_median = Median(consecutive_arr, n, k);
+            Console.WriteLine("An array of CONSECUTIVE NUMBERS: { 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 }");
+            Console.WriteLine("Median of the array is: {0}", consec_median);
+
+            Console.WriteLine();
+
+            //Array of Sorted Numbers
+            int[] sorted_arr = new int[] { 5, 7, 11, 13, 15, 17, 20, 23, 27, 29 };
+            int sorted_median = Median(sorted_arr, n, k);
+            Console.WriteLine("An array of SORTED NUMBERS: { 5, 7, 11, 13, 15, 17, 20, 23, 27, 29}");
+            Console.WriteLine("Median of the array is: {0}", sorted_median);
+
+            Console.WriteLine();
+
+            //Array of Unsorted Numbers
+            int[] unsorted_arr = new int[] { 20, 23, 7, 5, 11, 15, 29, 27, 13, 17 };
+            int unsorted_median = Median(unsorted_arr, n, k);
+            Console.WriteLine("An array of UNSORTED NUMBERS: { 20, 23, 7, 5, 11, 15, 29, 27, 13, 17 }");
+            Console.WriteLine("Median of the array is: {0}", unsorted_median);
+
+            Console.WriteLine();
+
+            //Array of Reverse Numbers
+            int[] reverse_arr = new int[] { 29, 27, 23, 20, 17, 15, 13, 11, 7, 5 };
+            int reverse_median = Median(reverse_arr, n, k);
+            Console.WriteLine("An array of REVERSE NUMBERS: { 29, 27, 23, 20, 17, 15, 13, 11, 7, 5 }");
+            Console.WriteLine("Median of the array is: {0}", reverse_median);
+
+            Console.WriteLine();
+
+            //Array of Random Numbers
+            int[] random_arr = new int[] { 44, 12, 32, 22, 1, 23, 6, 49, 11, 43 };
+            int random_median = Median(random_arr, n, k);
+            Console.WriteLine("An array of RANODM NUMBERS: { 44, 12, 32, 22, 1, 23, 6, 49, 11, 43 }");
+            Console.WriteLine("Median of the array is: {0}", random_median);
+
+            Console.WriteLine();
+
+            //Array of Partially Sorted Numbers
+            int[] partial_arr = new int[] { 13, 15, 17, 19, 20, 45, 11, 34, 31, 14 };
+            int partial_median = Median(partial_arr, n, k);
+            Console.WriteLine("An array of PARTIALLY SORTED NUMBERS: { 13, 15, 17, 19, 20, 45, 11, 34, 31, 22 }");
+            Console.WriteLine("Median of the array is: {0}", partial_median);
+
+            Console.WriteLine();
+
+            //Array of Odd Length
+            int[] odd_arr = new int[] { 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32 };
+            int odd_median = Median(odd_arr, 11, k);
+            Console.WriteLine("An array of ODD LENGTH: { 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32 }");
+            Console.WriteLine("Median of the array is: {0}", odd_median);
+            Console.WriteLine();
+
+            //Array Including Both positive and Negative Numbers
+            int[] mixed_arr = new int[] { -12, 34, 54, -99, -34, -7, 89, 234, -119, 37 };
+            int mixed_median = Median(mixed_arr, n, k);
+            Console.WriteLine("An array of PARTIALLY SORTED NUMBERS: { -12,34,54,-99,-34,-7,89,234,-119,37}");
+            Console.WriteLine("Median of the array is: {0}", mixed_median);
+        }
+
+    }
+}
+
+
