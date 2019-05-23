47e3c41a7fb2953d810729a1bb056127396f0d9f
Arik Intenam Mir <mirarikintenam@gmail.com>
2019-05-24 01:04:50 +1000

    the algorithm

---
+++ Brute_force_median_algorithm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brute_force_median_algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Median(A);
+            Console.WriteLine(Median(A));
+        }
+
+        public static int Median(int[] A)
+        {
+            int n = A.Length;
+            int k = n / 2;
+
+            for (int i = 0; i <= (n - 1); i++)
+            {
+                int numsmaller = 0;
+                int numequal = 0;
+
+                for (int j = 0; j <= (n - 1); j++)
+                {
+
+                    if (A[j] < A[i])
+                    {
+
+                        numsmaller = numsmaller + 1;
+                    }
+                    else if (A[j] == A[i])
+                    {
+
+                        numequal = numequal + 1;
+                    }
+                }
+                if ((numsmaller < k) && (k <= (numsmaller + numequal)))
+                {
+                    return A[i];
+                }
+            }
+            return -1;
+        }
+
+
+    }
+}
